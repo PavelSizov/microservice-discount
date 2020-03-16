a420ed41fb25a3fb77fce177aa11f1c3d8cf49ac
PavelS <Pavelsizov13@gmail.com>
2020-03-16 17:33:24 +0300

    Added Retry for calls to external services

--- src/main/java/com/microservices/mentoring/discount/DiscountApplication.java
+++ src/main/java/com/microservices/mentoring/discount/DiscountApplication.java
@@ -2,8 +2,10 @@ package com.microservices.mentoring.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class DiscountApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/microservices/mentoring/discount/repository/PromocodeRepository.java
+++ src/main/java/com/microservices/mentoring/discount/repository/PromocodeRepository.java
@@ -2,6 +2,7 @@ package com.microservices.mentoring.discount.repository;
 
 import com.microservices.mentoring.discount.domain.Promocode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,6 +11,7 @@ public class PromocodeRepository {
     @Autowired
     private DynamoDBRepository dynamoDBRepository;
 
+    @Retryable
     public Promocode getPromocode(String code) {
         return dynamoDBRepository.findPromocode(Promocode.class, code);
     }
