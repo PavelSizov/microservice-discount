db05389ed87f39a0dd612815d5b542f207558c78
PavelS <Pavelsizov13@gmail.com>
2020-05-25 15:59:19 +0300

    Parametrized tablename

--- src/main/java/com/microservices/mentoring/discount/domain/Promocode.java
+++ src/main/java/com/microservices/mentoring/discount/domain/Promocode.java
@@ -7,7 +7,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import java.math.BigDecimal;
 import java.util.StringJoiner;
 
-@DynamoDBTable(tableName = "Promocode")
+@DynamoDBTable(tableName = "${db_table}")
 public class Promocode {
 
     @DynamoDBHashKey(attributeName = "code")
