84e57cbf435fc81a3695d9a83b4fb4382cc16290
PavelS <Pavelsizov13@gmail.com>
2020-06-03 20:28:18 +0300

    Added cross-origin permission

--- src/main/java/com/microservices/mentoring/discount/controller/PromocodeController.java
+++ src/main/java/com/microservices/mentoring/discount/controller/PromocodeController.java
@@ -2,11 +2,13 @@ package com.microservices.mentoring.discount.controller;
 
 import com.microservices.mentoring.discount.service.PromocodeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class PromocodeController {
 
     @Autowired
